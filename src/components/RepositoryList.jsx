7be7a376cfcd9fe76204d830bec529d25f23f052
vyctoria karina <vyctoriak@gmail.com>
2022-03-02 16:34:27 +0100

    add react-refresh to the webpack file, repository list styling

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../styles/repositories.scss';
+
 const repository = {
   name: 'Unform22',
   description: 'Example of a repository description',
